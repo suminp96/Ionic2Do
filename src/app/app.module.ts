afc8389665ae63ed2e263eccb923fcf7dcc04204
suminp96 <dreamer3ds@gmail.com>
2017-09-26 13:12:21 +0900

    database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { TaskListPage } from '../pages/tasklist/tasklist';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCA-2GzAJgE1DJHse9d_KHuThoL8JqtWVw",
+  authDomain: "ionic2do-2d5bb.firebaseapp.com",
+  databaseURL: "https://ionic2do-2d5bb.firebaseio.com",
+  projectId: "ionic2do-2d5bb",
+  storageBucket: "ionic2do-2d5bb.appspot.com",
+  messagingSenderId: "887601734221"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +26,9 @@ import { TaskListPage } from '../pages/tasklist/tasklist';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ItemSliding } from 'ionic-angular';
+import { Task } from "./task";
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-tasklist',
   templateUrl: 'tasklist.html'
 })
 export class TaskListPage {
-  tasks: Array<any> = [];
-  constructor(public navCtrl: NavController) {
-    this.tasks = [
-      {title: 'Milk', status: 'open'},
-      {title: 'Eggs', status: 'open'},
-      {title: 'Syrup', status: 'open'},
-      {title: 'Pancake Mix', status: 'open'}
-    ];
+  tasks: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public af: AngularFireDatabase) {
+    this.tasks = af.list('/tasks');
   }
   addItem(){
     let theNewTask: string = prompt("New Task");
     if (theNewTask !== ''){
       this.tasks.push({ title: theNewTask, status: 'open'});
     }
+  }
+  markAsDone(slidingItem: ItemSliding, task: any){
+    this.tasks.update(task.$key, { status: 'done'});
+    slidingItem.close();
+  }
+  removeTask(slidingItem: ItemSliding, task: any){
+    this.tasks.remove(task.$key);
+    slidingItem.close();
   }
 
 }
